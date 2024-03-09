b93a3cd206a7621f9c41182fc99c442dfb0c8328
dfense23 <71448852+dfense23@users.noreply.github.com>
2024-03-09 17:27:41 +0100

    remove mouse move from layer numpad, add mousebuttons to homerow
     for automated change to this layer by mouse usage.

--- keyboards/splitkb/kyria/keymaps/dfense23/keymap.c
+++ keyboards/splitkb/kyria/keymaps/dfense23/keymap.c
@@ -127,9 +127,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_WBAK, KC_WFWD, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS,       KC_NUBS, LSFT(KC_7), RALT(KC_NUBS), LSFT(KC_NUBS), RALT(KC_NO), KC_TRNS, 
                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 	[_NUMPAD] = LAYOUT_split_3x6_5(
-        KC_TRNS, KC_TRNS, KC_BTN2, KC_MS_U, KC_BTN1, KC_PGUP,                                                               KC_NUM, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PMNS, 
-        KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_PGDN,                                                               KC_DOWN, KC_P4, KC_P5, KC_P6, KC_PAST, KC_PSLS, 
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN3, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS,       KC_RGHT, KC_P1, KC_P2, KC_P3, KC_PENT, KC_BSPC, 
+        KC_TRNS, LCTL(KC_T), LCTL(KC_W), LCTL(KC_F), KC_PGUP,                                                               KC_NUM, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PMNS, 
+        KC_WBAK, KC_WFWD, KC_BTN2, KC_BTN3, KC_BTN1, KC_PGDN,                                                               KC_DOWN, KC_P4, KC_P5, KC_P6, KC_PAST, KC_PSLS, 
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS,       KC_RGHT, KC_P1, KC_P2, KC_P3, KC_PENT, KC_BSPC, 
                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                              KC_TRNS, KC_TRNS, KC_TAB, KC_TRNS, KC_PDOT),
 	[_GAME] = LAYOUT_split_3x6_5(
         KC_B, KC_TAB, KC_G, KC_W, KC_F, KC_R,                                                                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
